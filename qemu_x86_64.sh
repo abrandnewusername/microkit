2c51872e1f7471f789568fe9f7e590495b0dc40e
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-02-14 16:00:44 +1100

    Add convenience scripts for testing x86 stuff

---
+++ qemu_x86_64.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+./pyenv/bin/python dev_build.py --board x86_64_virt --example hello --rebuild
+objcopy -O elf32-i386 release/microkit-sdk-1.2.6/board/x86_64_virt/debug/elf/sel4.elf tmp_build/kernel.elf
+
+qemu-system-x86_64                                                                         \
+        -cpu Nehalem,-vme,+pdpe1gb,-xsave,-xsaveopt,-xsavec,+fsgsbase,-invpcid,+syscall,+lm,enforce \
+        -m "3G"                                                                             \
+        -display none                                                                       \
+        -serial mon:stdio                                                                   \
+	-kernel tmp_build/kernel.elf \
+	-initrd tmp_build/capdl-initialiser-with-spec.elf
---
+++ vb105.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+set -e
+
+./pyenv/bin/python dev_build.py --board vb_105 --example hello --rebuild
+objcopy -O elf32-i386 release/microkit-sdk-1.2.6/board/x86_64_virt/debug/elf/sel4.elf tmp_build/kernel.elf
+
