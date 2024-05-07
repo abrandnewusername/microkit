ffe7b9202bf72b835489a4c1450e5eb3725d0bd5
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-05-07 16:37:19 +1000

    Remove quick-xml

--- tool/microkit-rs/src/sysxml.rs
+++ tool/microkit-rs/src/sysxml.rs
@@ -1,6 +1,4 @@
 use std::path::{Path, PathBuf};
-use quick_xml::de::Deserializer;
-use serde::{Deserialize};
 use crate::sel4::{PageSize, ArmIrqTrigger, KernelConfig, KernelArch};
 
 ///
@@ -54,66 +52,6 @@ impl PlatformDescription {
             page_sizes,
         }
     }
-}
-
-#[derive(Deserialize, Debug, PartialEq)]
-struct XmlSysIrq {
-    #[serde(rename = "@irq")]
-    irq: String,
-    #[serde(rename = "@id")]
-    id: String,
-    #[serde(rename = "@trigger")]
-    trigger: Option<String>,
-}
-
-#[derive(Deserialize, Debug, PartialEq)]
-struct XmlProgramImage {
-    #[serde(rename = "@path")]
-    path: String,
-}
-
-#[derive(Deserialize, Debug, PartialEq)]
-struct XmlChannelEnd {
-    #[serde(rename = "@pd")]
-     pub pd: String,
-    #[serde(rename = "@id")]
-     pub id: String,
-}
-
-#[derive(Deserialize, Debug, PartialEq)]
-struct XmlChannel {
-    #[serde(rename = "@name")]
-    pub ends: Vec<XmlChannelEnd>,
-}
-
-#[derive(Deserialize, Debug, PartialEq)]
-struct XmlMemoryRegion {
-    #[serde(rename = "@name")]
-    pub name: String,
-    #[serde(rename = "@size")]
-    pub size: String,
-    #[serde(rename = "@page_size")]
-    pub page_size: Option<String>,
-    #[serde(rename = "@phys_addr")]
-    pub phys_addr: Option<String>,
-}
-
-#[derive(Deserialize, Debug, PartialEq)]
-struct XmlProtectionDomain {
-    #[serde(rename = "@name")]
-    pub name: String,
-    #[serde(rename = "@priority")]
-    pub priority: String,
-    pub program_image: XmlProgramImage,
-    #[serde(rename = "irq")]
-    pub irqs: Option<Vec<XmlSysIrq>>,
-}
-
-#[derive(Deserialize, Debug, PartialEq)]
-struct XmlSystemDescription {
-    pub protection_domain: Vec<XmlProtectionDomain>,
-    pub memory_region: Option<Vec<XmlMemoryRegion>>,
-    pub channel: Option<Vec<XmlChannel>>,
 }
 
 #[repr(u8)]
@@ -183,38 +121,7 @@ pub struct ProtectionDomain {
 }
 
 impl ProtectionDomain {
-    fn from_xml(xml: XmlProtectionDomain) -> ProtectionDomain {
-        let budget = 1000;
-        let period = budget;
-        let pp = false;
-        let passive = false;
-        let program_image = Path::new(&xml.program_image.path).to_path_buf();
-        let maps = vec![];
-        let irqs;
-        if let Some(xml_irqs) = xml.irqs {
-            irqs = xml_irqs.into_iter().map(|irq| SysIrq::from_xml(irq)).collect();
-        } else {
-            irqs = vec![];
-        }
-        let setvars = vec![];
-
-        // TODO: need to check that parse works as expected. I don't know
-        // what format it expects (hex, decimal, binary etc)
-        ProtectionDomain {
-            name: xml.name,
-            priority: xml.priority.parse::<u32>().unwrap(),
-            budget,
-            period,
-            pp,
-            passive,
-            program_image,
-            maps,
-            irqs,
-            setvars,
-        }
-    }
-
-    fn from_rxml(xml: &roxmltree::Node) -> ProtectionDomain {
+    fn from_xml(xml: &roxmltree::Node) -> ProtectionDomain {
         let budget = 1000;
         let period = budget;
         let pp = false;
@@ -267,88 +174,88 @@ impl ProtectionDomain {
 }
 
 impl SysIrq {
-    fn from_xml(xml: XmlSysIrq) -> SysIrq {
-        // TODO: remove ARM specific trigger and use general thing
-        let trigger;
-        if let Some(xml_trigger) = xml.trigger {
-            trigger = match xml_trigger.as_str() {
-                "level" => ArmIrqTrigger::Level,
-                "edge" => ArmIrqTrigger::Edge,
-                _ => panic!("trigger must be either 'level' or 'edge'")
-            }
-        } else {
-            // Default to level triggered
-            trigger = ArmIrqTrigger::Level;
-        }
+    // fn from_xml(xml: XmlSysIrq) -> SysIrq {
+    //     // TODO: remove ARM specific trigger and use general thing
+    //     let trigger;
+    //     if let Some(xml_trigger) = xml.trigger {
+    //         trigger = match xml_trigger.as_str() {
+    //             "level" => ArmIrqTrigger::Level,
+    //             "edge" => ArmIrqTrigger::Edge,
+    //             _ => panic!("trigger must be either 'level' or 'edge'")
+    //         }
+    //     } else {
+    //         // Default to level triggered
+    //         trigger = ArmIrqTrigger::Level;
+    //     }
 
-        // TODO: need to actually handle error in case it's not a u64
-        let irq = xml.irq.parse::<u64>().unwrap();
-        let id = xml.id.parse::<u64>().unwrap();
+    //     // TODO: need to actually handle error in case it's not a u64
+    //     let irq = xml.irq.parse::<u64>().unwrap();
+    //     let id = xml.id.parse::<u64>().unwrap();
 
-        SysIrq {
-            irq,
-            id,
-            trigger,
-        }
-    }
+    //     SysIrq {
+    //         irq,
+    //         id,
+    //         trigger,
+    //     }
+    // }
 }
 
 impl SysMemoryRegion {
-    fn from_xml(xml: XmlMemoryRegion, plat_desc: &PlatformDescription) -> SysMemoryRegion {
-        let page_size = if let Some(xml_page_size) = xml.page_size {
-            xml_page_size.parse::<u64>().unwrap()
-        } else {
-            plat_desc.page_sizes[0]
-        };
+    // fn from_xml(xml: XmlMemoryRegion, plat_desc: &PlatformDescription) -> SysMemoryRegion {
+    //     let page_size = if let Some(xml_page_size) = xml.page_size {
+    //         xml_page_size.parse::<u64>().unwrap()
+    //     } else {
+    //         plat_desc.page_sizes[0]
+    //     };
 
-        // TODO: check valid
-        let page_size_valid = plat_desc.page_sizes.contains(&page_size);
+    //     // TODO: check valid
+    //     let page_size_valid = plat_desc.page_sizes.contains(&page_size);
 
-        let phys_addr = if let Some(xml_phys_addr) = xml.phys_addr {
-            Some(xml_phys_addr.parse::<u64>().unwrap())
-        } else {
-            None
-        };
+    //     let phys_addr = if let Some(xml_phys_addr) = xml.phys_addr {
+    //         Some(xml_phys_addr.parse::<u64>().unwrap())
+    //     } else {
+    //         None
+    //     };
 
-        let page_count = 0; // TODO
+    //     let page_count = 0; // TODO
 
-        SysMemoryRegion {
-            name: xml.name,
-            size: xml.size.parse::<u64>().unwrap(),
-            page_size: page_size.into(),
-            page_count,
-            phys_addr,
-        }
-    }
+    //     SysMemoryRegion {
+    //         name: xml.name,
+    //         size: xml.size.parse::<u64>().unwrap(),
+    //         page_size: page_size.into(),
+    //         page_count,
+    //         phys_addr,
+    //     }
+    // }
 }
 
 impl Channel {
-    fn from_xml(xml: XmlChannel, pds: &Vec<ProtectionDomain>) -> Channel {
-        // TODO: what if ends is empty?
-        let pd_a = pds.iter().position(|pd| pd.name == xml.ends[0].pd).unwrap();
-        let id_a = xml.ends[0].id.parse::<u64>().unwrap();
+    // fn from_xml(xml: XmlChannel, pds: &Vec<ProtectionDomain>) -> Channel {
+    //     // TODO: what if ends is empty?
+    //     let pd_a = pds.iter().position(|pd| pd.name == xml.ends[0].pd).unwrap();
+    //     let id_a = xml.ends[0].id.parse::<u64>().unwrap();
 
-        let pd_b = pds.iter().position(|pd| pd.name == xml.ends[1].pd).unwrap();
-        let id_b = xml.ends[1].id.parse::<u64>().unwrap();
+    //     let pd_b = pds.iter().position(|pd| pd.name == xml.ends[1].pd).unwrap();
+    //     let id_b = xml.ends[1].id.parse::<u64>().unwrap();
 
-        if id_a > PD_MAX_ID {
-            panic!("id must be < {}", PD_MAX_ID + 1);
-        }
-        if id_b > PD_MAX_ID {
-            panic!("id must be < {}", PD_MAX_ID + 1);
-        }
+    //     if id_a > PD_MAX_ID {
+    //         panic!("id must be < {}", PD_MAX_ID + 1);
+    //     }
+    //     if id_b > PD_MAX_ID {
+    //         panic!("id must be < {}", PD_MAX_ID + 1);
+    //     }
 
-        if xml.ends.len() != 2 {
-            panic!("exactly two end elements must be specified")
-        }
+    //     if xml.ends.len() != 2 {
+    //         panic!("exactly two end elements must be specified")
+    //     }
 
-        Channel {
-            pd_a,
-            id_a,
-            pd_b,
-            id_b,
-        }
-    }
+    //     Channel {
+    //         pd_a,
+    //         id_a,
+    //         pd_b,
+    //         id_b,
+    //     }
+    // }
 }
 
 pub struct SystemDescription {
@@ -356,43 +263,6 @@ pub struct SystemDescription {
     pub memory_regions: Vec<SysMemoryRegion>,
     pub channels: Vec<Channel>,
 }
-
-impl<'a> SystemDescription {
-    fn from_xml(xml: XmlSystemDescription, plat_desc: PlatformDescription) -> SystemDescription {
-        let pds: Vec<ProtectionDomain> = xml.protection_domain
-                                        .into_iter()
-                                        .map(|pd| ProtectionDomain::from_xml(pd))
-                                        .collect();
-        let mrs = if let Some(xml_memory_regions) = xml.memory_region {
-            xml_memory_regions
-                .into_iter()
-                .map(|mr| SysMemoryRegion::from_xml(mr, &plat_desc))
-                .collect()
-        } else {
-            vec![]
-        };
-        let channels = if let Some(xml_channels) = xml.channel {
-            xml_channels
-                .into_iter()
-                .map(|ch| Channel::from_xml(ch, &pds))
-                .collect()
-        } else {
-            vec![]
-        };
-        SystemDescription {
-            protection_domains: pds,
-            memory_regions: mrs,
-            channels,
-        }
-    }
-}
-
-// pub fn parse(xml: &str, plat_desc: PlatformDescription) -> SystemDescription {
-//     let mut deserializer = Deserializer::from_str(xml);
-//     let system_xml = XmlSystemDescription::deserialize(&mut deserializer).unwrap();
-
-//     SystemDescription::from_xml(system_xml, plat_desc)
-// }
 
 pub fn parse(xml: &str, plat_desc: PlatformDescription) -> SystemDescription {
     let doc = roxmltree::Document::parse(xml).unwrap();
@@ -409,7 +279,7 @@ pub fn parse(xml: &str, plat_desc: PlatformDescription) -> SystemDescription {
 
             println!("{:?}", child);
             match child.tag_name().name() {
-                "protection_domain" => pds.push(ProtectionDomain::from_rxml(&child)),
+                "protection_domain" => pds.push(ProtectionDomain::from_xml(&child)),
                 _ => panic!("TODO")
             }
         }
